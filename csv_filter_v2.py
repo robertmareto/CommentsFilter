b2d2321c470b375b6efc8f4778541085705a0434
Robert Mareto <robert.mareto@edu.ufes.br>
2024-09-05 17:23:01 -0300

    feat: Improve CSV filtering script with post-only option

--- csv_filter_v2.py
+++ csv_filter_v2.py
@@ -93,8 +93,10 @@ def menu() -> tuple:
         choiceType = input("Digite o número da opção desejada: ")
         if choiceType == '1':
             row_name = row_name_social
+            post_only = bool(False)
         elif choiceType == '2':
             row_name = row_name_user
+            post_only = bool(True)
         else:
             print("Por favor, escolha uma opção válida.")
     
@@ -115,7 +117,7 @@ def menu() -> tuple:
 
     terms_file = os.path.join(directory, json_file)
     dataset_file = os.path.join(directory, csv_file)
-    return row_name, terms_file, dataset_file, metadata_columns
+    return row_name, terms_file, dataset_file, metadata_columns, post_only
     
 def build_set(term_file:str) -> set:
     """Build a set of terms from a JSON file.
@@ -151,7 +153,7 @@ def build_list(term_file:str) -> list:
             term_list.append(term)
     return term_list
 
-def build_filtered_df(raw: DataFrame, terms_list: list, row_name: str, metadata_columns: list) -> DataFrame:
+def build_filtered_df(raw: pd.DataFrame, terms_list: list, row_name: str, metadata_columns: list, post_only: bool) -> pd.DataFrame:
     """Find all the matching terms in a DataFrame row.
     - Complexity: O(n^2) -> O(n) for each row.
     - Input: 
@@ -178,32 +180,45 @@ def build_filtered_df(raw: DataFrame, terms_list: list, row_name: str, metadata_
             normalized_terms.append(unidecode(term.lower()))
 
     for index, row in raw.iterrows():
+        if post_only and row['Type'] != 'Post':
+            raw.at[index, 'MatchTerm'] = 'Comment dont delete'
+            #print(f"Skipping row {index} as it is not a Post")
+            continue  # Continue to the next row
+
         # Normalize and split the row text
         words = [unidecode(word.lower()) for word in row[row_name].split()]
         for i, word in enumerate(words):
-            words[i] = re.sub(r'[^a-zA-Z0-9#@]', '', word)
+            words[i] = re.sub(r'[^a-zA-Z0-9#@_]', '', word)
        
-        print(f"{[index]} => {words}")
-
         if not words:
             continue
         first_word = words[0]
 
-        # build the trie with the post words
+        # Build the trie with the post words
         tst = Trie(first_word)
         for word in words[1:]:
             tst.append(word)
 
-        # check if the terms set contains all of the words in the post
+        # Check if the terms set contains all of the words in the post
         for term in normalized_terms:
             contain_all = True
             if isinstance(term, list):
+                match_words = []  # List of matched words
                 for t in term:
                     if not tst.__contains__(t):
                         contain_all = False
                         break
+
                 if contain_all:
-                    matching_terms.append(', '.join(term))
+                    same_order = True
+                    # Check if all terms are in the same order
+                    first_token_order = words.index(term[0])
+                    second_token_order = words.index(term[1])
+                    if second_token_order < first_token_order:
+                        same_order = False
+                    if same_order:
+                        matching_terms.append(', '.join(term))
+
             elif tst.__contains__(term):
                 matching_terms.append(term)
         if matching_terms:
@@ -211,16 +226,33 @@ def build_filtered_df(raw: DataFrame, terms_list: list, row_name: str, metadata_
             raw.at[index, 'MatchTerm'] = matching_terms
         del tst
         matching_terms = []
+
+    # Filter out the rows where 'MatchTerm' is NaN
+    filtered_df = raw[raw['MatchTerm'].notnull()]
+
+    if post_only:
+        # Get Tweet IDs of the filtered Posts
+        tweet_ids = filtered_df.loc[filtered_df['Type'] == 'Post', 'Tweet ID (click to view url)'].tolist()
         
-    return raw[raw['MatchTerm'].notnull()]
+        # Filter out Comments with SourcePostId matching any of the Tweet IDs
+        comments_to_remove = raw[(raw['Type'] == 'Comment') & (raw['SourcePostId'].isin(tweet_ids))]
+        
+        # Print the IDs of comments being removed
+        for comment_id in comments_to_remove['SourcePostId'].unique():
+            print(f"Removing Comment with SourcePostId: {comment_id}")
+        
+        # Remove Comments that are associated with the Tweet IDs
+        filtered_df = filtered_df[~((filtered_df['Type'] == 'Comment') & (filtered_df['SourcePostId'].isin(tweet_ids)))]
+
+    return filtered_df
    
 if  __name__ == '__main__':
-    row_name, terms_file, dataset_file, metadata_columns = menu()
+    row_name, terms_file, dataset_file, metadata_columns, post_only = menu()
     init = time.time() # benchmarking
     raw = pd.read_csv(dataset_file, sep=',')
     terms_list = build_list(terms_file)
     print('Filtrando...')
     filename = dataset_file.split('/')[-1].split('.')[0]
-    filtered_df = build_filtered_df(raw, terms_list, row_name, metadata_columns)
+    filtered_df = build_filtered_df(raw, terms_list, row_name, metadata_columns, post_only)
     filtered_df.to_csv(f'{filename}_filtered.csv', sep=',', header=True, index=False)
     print(f'Tempo de execução: {time.time() - init :.2f} segundos.\nTotal de linhas no dataset: {len(raw)}\nTotal de linhas filtradas: {len(filtered_df)}')
